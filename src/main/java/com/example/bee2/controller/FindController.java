2048a20641015477846329aed4fd0eb83938f005
0Kate <o.keito317@gmail.com>
2018-11-26 18:48:08 +0900

    > remake release form > change userlist page's navbar

--- src/main/java/com/example/bee2/controller/FindController.java
+++ src/main/java/com/example/bee2/controller/FindController.java
@@ -1,5 +1,7 @@
 package com.example.bee2.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,8 +17,9 @@ public class FindController {
 	private UserService userService;
 	
 	@RequestMapping(value="/bee/find", method=RequestMethod.GET)
-	public String findUser(@RequestParam String username, Model model) {
-		model.addAttribute("userList", userService.findByNameLike(username));
-		return "userlist";
+	public String findUser(@RequestParam String username, Model model, Principal principal) {
+	  model.addAttribute("user", userService.pickupUser(principal));
+	  model.addAttribute("userList", userService.findByNameLike(username));
+	  return "userlist";
 	}
 }
--- src/main/java/com/example/bee2/controller/TopController.java
+++ src/main/java/com/example/bee2/controller/TopController.java
@@ -3,19 +3,21 @@ package com.example.bee2.controller;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.example.bee2.entity.UserInfo;
+import com.example.bee2.entity.User;
 import com.example.bee2.form.PostForm;
 import com.example.bee2.service.MessageService;
 import com.example.bee2.service.PostService;
+import com.example.bee2.service.UserService;
 
 @Controller
 public class TopController {
+    @Autowired
+    private UserService userService;
 	@Autowired
 	private PostService postService;
 	@Autowired
@@ -23,13 +25,11 @@ public class TopController {
 	
 	@RequestMapping(value="/bee/top", method=RequestMethod.GET)
 	public String topPage(Model model, Principal principal) {
-		Authentication auth = (Authentication)principal;
-		UserInfo user = (UserInfo) auth.getPrincipal();
-		
-		model.addAttribute("user", user.getUser());
+	    User user = userService.pickupUser(principal);
+		model.addAttribute("user", user);
 		model.addAttribute("postList", postService.getAllPost());
 		model.addAttribute("postForm", new PostForm());
-		model.addAttribute("messageList", messageService.findByReciever(user.getUsername()));
+		model.addAttribute("messageList", messageService.findByReciever(user.getName()));
 		
 		return "top";
 	}
