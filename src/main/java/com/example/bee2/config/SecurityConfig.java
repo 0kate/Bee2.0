5acc2082f1bd46907a654f37949863f6bf8edc5d
0Kate <o.keito317@gmail.com>
2018-11-26 19:56:27 +0900

    update

--- src/main/java/com/example/bee2/config/SecurityConfig.java
+++ src/main/java/com/example/bee2/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.formLogin().loginPage("/bee/login").defaultSuccessUrl("/bee/top").failureUrl("/bee/login?error=true").permitAll().and()
 					.logout().logoutRequestMatcher(new AntPathRequestMatcher("/bee/logout")).logoutSuccessUrl("/bee/login").permitAll();
-		http.authorizeRequests().antMatchers("/html/**", "/css/**", "/js/**", "/assets/**", "/bee/login/**", "/bee/regist/**").permitAll()
+		http.authorizeRequests().antMatchers("/css/**", "/js/**", "/assets/**", "/bee/login/**", "/bee/regist/**").permitAll()
 					.antMatchers("/bee/admin/**").hasRole("ADMIN")
 					.anyRequest().authenticated();
 	}
