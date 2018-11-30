89b5dde353c780fd46a0fef898007d04d0544b1e
0Kate <o.keito317@gmail.com>
2018-11-30 15:00:02 +0900

    add auto generate user service

--- src/main/java/com/example/bee2/controller/AutoGenerateController.java
+++ src/main/java/com/example/bee2/controller/AutoGenerateController.java
@@ -1,19 +1,14 @@
 package com.example.bee2.controller;
 
-import java.util.Optional;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.bee2.form.RegistForm;
 import com.example.bee2.service.UserService;
 
 @Controller
@@ -21,21 +16,39 @@ public class AutoGenerateController {
 	@Autowired
 	private UserService userService;
 	
-	@RequestMapping(value="/bee/autogenerate", method=RequestMethod.GET)
-	public String autogeneratePage(Model model) {
-		model.addAttribute("registForm", new RegistForm());
-		return "autoGenerate";
+	@RequestMapping(value="/bee/autogenerate/regist", method=RequestMethod.GET)
+	public String autogenerateUser(@RequestParam(name="userCount", required=false) String count) {
+		String nextPath = null;
+		
+		if (StringUtils.isEmpty(count)) {
+			nextPath = "autogenerate";
+		} else {
+			String userName = "User" + count;
+			userService.registerUser(userName, 20L, userName + "@bee.com", "password", "Japan");
+			
+			nextPath = "redirect:/bee/autogenerate/regist?userCount=" + Integer.toString(Integer.parseInt(count) + 1);
+		}
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		return nextPath;
 	}
 	
-	@RequestMapping(value="/bee/autogenerate", method=RequestMethod.POST)
-	public String autogenerate(@CookieValue(name="userCount", required=false) String count, @ModelAttribute("registForm") RegistForm registForm, HttpServletResponse response) {
-		userService.registerUser(registForm.getUsername(), registForm.getAge(), registForm.getEmail(), registForm.getPassword(), registForm.getLocation());
+	@RequestMapping(value="/bee/autogenerate/follow", method=RequestMethod.GET)
+	public String autogenerateFollow(@RequestParam(name="fromUser", required=false) String fromUser, @RequestParam(name="toUser", required=false) String toUser) {
+		Random randGenerator = new Random();
 		
-		Optional<String> countOptional = Optional.ofNullable(count);
-		count = countOptional.map(cnt -> Integer.toString(Integer.parseInt(cnt) + 1)).orElseGet(() -> "0");
+		if (!StringUtils.isEmpty(fromUser) || !StringUtils.isEmpty(toUser)) {
+			 userService.follow(fromUser, toUser);
+		}
 		
-		response.addCookie(new Cookie("userCount", count));
+		String nextFromUser = "User" + (randGenerator.nextInt(100) + 1);
+		String nextToUser = "User" + (randGenerator.nextInt(100) + 1);
 		
-		return "redirect:/bee/autogenerate";
+		return "redirect:/bee/autogenerate/follow?fromsuer=" + nextFromUser + "&touser=" + nextToUser;
 	}
 }
